3aad1ac8456c80bc6473dde9993421b0980c0c7b
Paul Diaz <paulcardozo_01@outlook.com>
2021-07-27 00:24:06 -0500

    Se modifica el acceso de las rutas

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,10 +3,23 @@ const routes = [
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: "", component: () => import("src/pages/Home.vue") },
-      { path: "/Tasks", component: () => import("src/pages/Tasks.vue") },
-      { path: "/Register", component: () => import("pages/RegisterUser.vue") },
-      { path: "/LogIn", component: () => import("pages/LogInUser.vue") }
+      {
+        path: "",
+        component: () => import("src/pages/Home.vue"),
+      },
+      {
+        path: "/Tasks",
+        component: () => import("src/pages/Tasks.vue"),
+        meta: { protect: true }
+      },
+      {
+        path: "/Register",
+        component: () => import("pages/RegisterUser.vue")
+      },
+      {
+        path: "/LogIn",
+        component: () => import("pages/LogInUser.vue")
+      }
     ]
   },
 
